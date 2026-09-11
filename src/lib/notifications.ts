3a5446f351bb2939d30e43fd62b2f02d92e1a276
princetrump7 <princetrump7@gmail.com>
2026-09-11 15:57:10 +0000

    chore: upgrade to Expo SDK 57 (expo 57.0.22, RN 0.86.3, React 19.2.3, notifications API update)

--- src/lib/notifications.ts
+++ src/lib/notifications.ts
@@ -3,7 +3,8 @@ import { daysLeft } from "./expiry";
 
 Notifications.setNotificationHandler({
   handleNotification: async () => ({
-    shouldShowAlert: true,
+    shouldShowBanner: true,
+    shouldShowList: true,
     shouldPlaySound: true,
     shouldSetBadge: false,
   }),
